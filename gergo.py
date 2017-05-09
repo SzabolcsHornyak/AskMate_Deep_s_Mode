5b66e4c4c21044b80e4663fe1dce0b348c542abf
Gergo Szabo <gergo.szabo23@gmail.com>
2017-05-10 01:01:22 +0200

    Display one question with all its attributes done

---
+++ gergo.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import base64
+import time
+app = Flask(__name__, static_url_path='/static')
+
+
+def decode_this(string):
+    return base64.b64decode(string).decode('utf-8')
+
+
+@app.route('/question/<int:id>')
+def question(id):
+    fieldnames = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image', 'edit', 'delete']
+    with open('./static/data/question.csv', 'r') as qcsvfile:
+        data_set = [line.split(',') for line in qcsvfile.readlines()]
+        for line in data_set:
+            line[1] = time.ctime(int(line[1]))
+            line[4] = decode_this(line[4])
+            line[5] = decode_this(line[5])
+            line[6] = decode_this(line[6])
+
+        data_set = data_set[id]
+
+        return render_template('display.html', data_set=data_set, fieldnames=fieldnames)
+
+
+def main():
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
