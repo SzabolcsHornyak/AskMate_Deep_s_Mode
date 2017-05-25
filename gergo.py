2c6bcf30c4132f871e0421da98ab591ca3d77ed2
Gergo Szabo <gergo.szabo23@gmail.com>
2017-05-25 15:59:08 +0200

    Sort is finally figured out

--- gergo.py
+++ gergo.py
@@ -1,17 +1,19 @@
 @app.route('/list')
 def list():
-    data_set = execute_sql_statement("SELECT * FROM question ORDER BY submission_time DESC;")
+    data_set = execute_sql_statement("SELECT * FROM question order by submission_time DESC;")
     sort_direction = 'asc'
-    dict_from_query_string = request.args
-    column = dict_from_query_string.keys()
-
+    list_from_query_string = request.query_string.decode('utf-8').split('=')
     try:
-        if request.args.values() == 'asc':
-            sort_direction = 'dsc'
-            execute_sql_statement('SELECT * FROM question ORDER BY %s DESC;', (column,))
-        elif request.args.values() == 'dsc':
-            sort_direction = 'asc'
-            execute_sql_statement('SELECT * FROM question ORDER BY %s ASC;', (column,))
+        if len(list_from_query_string) == 2:
+            column = list_from_query_string[0].encode('utf-8')
+            if str(list_from_query_string[1]) == 'asc':
+                sort_direction = 'dsc'
+                data_set = execute_sql_statement("SELECT * FROM question ORDER BY %s DESC;", (column,))
+
+            elif str(list_from_query_string[1]) == 'desc':
+                sort_direction = 'asc'
+                data_set = execute_sql_statement("SELECT * FROM question ORDER BY %s ASC;", (column,))
+
     except IndexError:
         pass
     return render_template('list.html', data_set=data_set, fieldnames=constants.FIELDNAMES, dir=sort_direction)
