d880d891ecd9a51d330a3c559f8a13b1650a4e0a
Gergo Szabo <gergo.szabo23@gmail.com>
2017-05-25 17:12:21 +0200

    Sort moved to main.py

--- main.py
+++ main.py
@@ -24,18 +24,20 @@ def get_list_5():
 def list():
     data_set = execute_sql_statement("SELECT * FROM question order by submission_time DESC;")
     sort_direction = 'asc'
-    '''
     list_from_query_string = request.query_string.decode('utf-8').split('=')
-    if len(list_from_query_string) == 2:
-        b = list_from_query_string[0]
-        if str(list_from_query_string[1]) == 'asc':
-            sort_direction = 'dsc'
-            execute_sql_statement("SELECT * FROM question ORDER BY %s DESC;", (b,))
-        elif str(list_from_query_string[1]) == 'dsc':
-            sort_direction = 'asc'
-            execute_sql_statement("SELECT * FROM question ORDER BY %s ASC;", (b,))
+    try:
+        if len(list_from_query_string) == 2:
+            column = list_from_query_string[0].encode('utf-8')
+            if str(list_from_query_string[1]) == 'asc':
+                sort_direction = 'dsc'
+                data_set = execute_sql_statement("SELECT * FROM question ORDER BY %s DESC;", (column,))
+
+            elif str(list_from_query_string[1]) == 'desc':
+                sort_direction = 'asc'
+                data_set = execute_sql_statement("SELECT * FROM question ORDER BY %s ASC;", (column,))
+
     except IndexError:
-        pass'''
+        pass
     return render_template('list.html', data_set=data_set, fieldnames=constants.FIELDNAMES, dir=sort_direction)
 
 
