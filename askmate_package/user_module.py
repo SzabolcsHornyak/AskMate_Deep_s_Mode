272d149929db97350cb73ddc52b80822b8daaadc
szabadon <zsoltiusz.nagy@gmail.com>
2017-06-07 15:36:58 +0200

    Changed query in get_user_list function to get all of the data of the user.

--- askmate_package/user_module.py
+++ askmate_package/user_module.py
@@ -6,5 +6,5 @@ def get_user_list():
     Gets a list of all users in the users table, returns them as a list of tuples
     where one tuple contains the dataset of one user.
     '''
-    users = execute_sql_statement('''SELECT username, registration_time, reputation FROM users;''')
+    users = execute_sql_statement('''SELECT * FROM users;''')
     return users
