b1c7e595c50d1fbbdb04c62de2495c56800005d5
akosENagy <akos.e.nagy@gmail.com>
2017-06-07 15:22:41 +0200

    Added docstring and repu change to set_to_accepted()

--- askmate_package/answer_module.py
+++ askmate_package/answer_module.py
@@ -1,8 +1,18 @@
 from askmate_package.db_handling import execute_sql_statement
+import user_module
 
 
 def set_to_accepted(answer_id):
-    qid = execute_sql_statement('''SELECT question_id FROM answer WHERE id=%s;''', (answer_id,))[0][0]
+    '''
+    Sets an answer to Accepted, all others to the same question to not accepted,
+    and updates the reputation of the user who posted the accepted answer.
+    '''
+    answer = execute_sql_statement('''SELECT * FROM answer WHERE id=%s;''', answer_id)[0]
+    qid = answer[3]
+    user_id = answer[7]
+
     execute_sql_statement('''UPDATE answer SET accepted=FALSE WHERE question_id=%s AND accepted=TRUE;''', (qid,))
     execute_sql_statement('''UPDATE answer SET accepted=TRUE WHERE id=%s;''', (answer_id,))
+    user_module.user_reputation(user_id, 15)
+
     return qid
